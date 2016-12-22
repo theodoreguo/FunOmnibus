b8fe86a00d1296289869e12538cd3a0cf21d9c94
Theodore Guo <theodoreguo@Theodores-MacBook-Pro.local>
2016-12-22 17:31:46 +0800

    Initial Commit

---
+++ MyWeibo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyWeibo
+//
+//  Created by Theodore Guo on 22/12/16.
+//  Copyright © 2016 Theodore Guo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyWeibo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MyWeibo
+//
+//  Created by Theodore Guo on 22/12/16.
+//  Copyright © 2016 Theodore Guo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
